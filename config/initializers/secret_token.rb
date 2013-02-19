b9b851eff6551cae7aff3f199719c6221d8c2dde
Brandon Cato <b.cato28@gmail.com>
2013-02-19 13:11:30 -0800

    Create a simple rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '845cd3cc23dc8fec869e10ba0c35abf80ce052de100e87961a855d914c1248d756151d82052a5aa921fe8836ba1ad5a35d14770766621b70f5fa1f455b044820'
---
+++ test/unit/user_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+	class UserTest < ActiveSupport::TestCase
+	 test "a user should enter a first name" do
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:first_name].empty?
+	 end
+
+	test "a user should enter a last name" do
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:last_name].empty?
+	 end
+
+	test "a user should enter a profile name" do
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:profile_name].empty?
+	 end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.profile_name = users(:brandon).profile_name
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new(first_name: 'brandon', last_name: 'cato', email: 'b.cato28@gmail.com')
+		user.password = user.password_confirmation = 'asdfasdf'
+
+		user.profile_name = "My Profile with Spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test " a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'brandon', last_name: 'cato', email: 'b.cato28@gmail.com')
+		user.password = user.password_confirmation = 'asdfasdf'
+
+		user.profile_name = 'bcato'
+		assert user.valid?
+	end
+end
+
